5d2249fc1ebce77b9f6287a1ea72c7704878d02c
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-07 17:13:32 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/Home/Home';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Banner />
+      <Footer />
+    </main>
+  );
+}
