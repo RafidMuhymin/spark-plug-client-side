bf108ef358e921afe362abe34c5d56d6e61ba48f
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-08 12:02:38 +0600

    added banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,24 @@
-import React from "react";
 import src from "../../images/ford.jpg";
+import "./Banner.css";
 
 export default function Banner() {
   return (
-    <section>
+    <section className="banner position-relative">
       <img
         src={src}
         className="img-fluid"
         alt="An electric car charging at an outlet"
       />
+
+      <div className="position-absolute w-100 h-100 top-0 start-0">
+        <span className="h2 position-absolute">Think Globally</span>
+
+        <div className="position-absolute">
+          <span className="h2">Act Locally, Electrically!</span>
+
+          <span className="h3">Spark Plug</span>
+        </div>
+      </div>
     </section>
   );
 }
