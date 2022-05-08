6399cb627ec64d98b91784e87af5a4ee092d71d3
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-08 15:52:16 +0600

    completed recent vehicles design

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -18,5 +18,26 @@ export default function Cars() {
     </div>
   );
 
-  return loadingSpinner;
+  return cars.length === 0 ? (
+    loadingSpinner
+  ) : (
+    <div className="cars-container d-flex align-items-stretch overflow-auto">
+      {cars.map(
+        ({ _id, name, price, description, quantity, image, supplier }) => {
+          return (
+            <div key={_id} className="p-3">
+              <article className="car h-100 p-3 rounded-3 d-flex flex-column justify-content-between">
+                <img src={image} alt={name} className="img-fluid" />
+                <h3>{name}</h3>
+                <p>{description}</p>
+                <span className="d-block">{price}</span>
+                <span className="d-block">{quantity}</span>
+                <span className="d-block">{supplier}</span>
+              </article>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
--- src/components/RecentVehicles/RecentVehicles.jsx
+++ src/components/RecentVehicles/RecentVehicles.jsx
@@ -3,11 +3,11 @@ import "./RecentVehicles.css";
 
 export default function RecentVehicles() {
   return (
-    <section className="recent-vehicles d-flex">
-      <div className="p-4 w-25 d-flex flex-column justify-content-center">
-        <h2 className="text-nowrap">Recent Vehicles</h2>
+    <section className="recent-vehicles bg-dark text-white p-4">
+      <div className="text-center px-4 mx-auto">
+        <h2>Recent Vehicles</h2>
 
-        <p className="text-center ms-3">
+        <p>
           Browse through our selection of excellent electric vehicles that have
           recently been added to our inventory.
         </p>
