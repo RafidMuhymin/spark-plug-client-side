88b0865783f52383be315facc185cfd8a2972323
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-09 19:38:41 +0600

    fixed spinner spacing issue

--- src/components/SpinnerBorder/SpinnerBorder.jsx
+++ src/components/SpinnerBorder/SpinnerBorder.jsx
@@ -1,7 +1,9 @@
 export default function SpinnerBorder() {
   return (
-    <div className="spinner-border text-light mb-0" role="status">
-      <span className="visually-hidden">Loading...</span>
+    <div>
+      <div role="status" className="spinner-border text-light d-block mx-auto">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 }
